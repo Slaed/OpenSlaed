686cd329876b97323ce6a17442a4425dd2e2ab0b
Slaed <os@mymobilka.net.ru>
2011-04-03 07:29:51 +0000

    Рейтинг +/- 1 для OpenSlaed

--- Рейтинг/config/config_header.php
+++ Рейтинг/config/config_header.php
@@ -4,5 +4,5 @@ if (!defined("FUNC_FILE")) die("Illegal File Access");
 echo PHP_EOL.'<link rel="stylesheet" type="text/css" href="images/rating/rating.css?'.filemtime("images/rating/rating.css").'" />';
 #Если у Вас уже подключена библиотека JQuery, то строку ниже можно удалить
 echo PHP_EOL.'<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>';
-echo PHP_EOL.'<script type="text/javascript" src="images/rating/rating.js?'.filemtime("ajax/rating.js").'"></script>';
+echo PHP_EOL.'<script type="text/javascript" src="images/rating/rating.js?'.filemtime("images/rating/rating.js").'"></script>';
 ?>
