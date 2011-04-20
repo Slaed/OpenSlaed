2347ce08b3816b5b9a8aebc2c84d42efaa7b520f
Slaed <os@mymobilka.net.ru>
2011-04-20 10:15:13 +0000

    Хак: Закрытие сайта

---
+++ Закрытие сайта/config/config_offline.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined("FUNC_FILE")) die("Illegal File Access");
+
+$offline['status'] = "0";
+$offline['type'] = "0";
+$offline['reason'] = "Временные проблемы у хостера";
+$offline['autorun'] = "1";
+$offline['close'] = "12/31/2010 18:25";
+$offline['open'] = "12/31/2011 20:45";
+
+?>
