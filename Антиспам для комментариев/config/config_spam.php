396a384f245541c2f015d1b940c83dab4c771044
Slaed <os@mymobilka.net.ru>
2011-04-27 03:25:29 +0000

    Антиспам для комментариев для OpenSlaed

--- Антиспам для комментариев/config/config_spam.php
+++ Антиспам для комментариев/config/config_spam.php
@@ -1,7 +1,7 @@
 <?php
 if (!defined("FUNC_FILE")) die("Illegal File Access");
 
-$spam['key']='0428dd9c4d3a';
+$spam['key']='Ваш ключ введите сюда';
 $spam['akismet']=0;
 $spam['url']=1;
 $spam['user']=0;
