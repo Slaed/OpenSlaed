1389413d64602b43cdf2e7a7a2901cf06f4b74ea
Slaed <os@mymobilka.net.ru>
2011-08-01 11:24:44 +0000

    Календарь

--- Календарь/config/config_header.php
+++ Календарь/config/config_header.php
@@ -2,6 +2,7 @@
 if (!defined("FUNC_FILE")) die("Illegal File Access");
 
 echo PHP_EOL.'<link rel="stylesheet" href="/images/calendar/calendar.css?'.filemtime("images/calendar/calendar.css").'"/>';
+#Если библиотека JQuery у вас уже подключена строку ниже можно удалить...
 echo PHP_EOL.'<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>';
 echo PHP_EOL.'<script type="text/javascript" src="/images/calendar/calendar.js?'.filemtime("images/calendar/calendar.js").'"></script>';
 ?>
