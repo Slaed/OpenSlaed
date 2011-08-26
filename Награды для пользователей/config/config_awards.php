b3a9d496a7a89532653655c46c8797eed46a4e20
Slaed <os@mymobilka.net.ru>
2011-08-26 03:04:52 +0000

    Хак: Награды

--- Награды для пользователей/config/config_awards.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (!defined("FUNC_FILE")) die("Illegal File Access");
-
-$awards['cats'] = array ();
-$awards['users'] = array ();
-
-?>
--- Награды для пользователей/config/config_header.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!defined("FUNC_FILE")) die("Illegal File Access");
-
-echo PHP_EOL.'<link rel="stylesheet" href="images/awards/awards.css?'.filemtime("images/awards/awards.css").'" type="text/css">';
-# Если у вас уже подключена библиотека JQuery, то строку ниже можно удалить
-echo PHP_EOL.'<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>';
-echo PHP_EOL.'<script type="text/javascript" src="images/awards/awards.js?'.filemtime("images/awards/awards.js").'"></script>';
-
-?>
